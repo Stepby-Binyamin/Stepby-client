2a719105c09e9a55e586104885cbd41e8f38c3cb
Yossef <yamar8@gmail.com>
2022-09-04 15:27:51 +0300

    my first commit

---
+++ src/components/common/BtnCheckBox/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./style.module.css";
+
+const BtnCheckBox = ({
+  onClick,
+  icon = "",
+  style = {},
+  text,
+  textColor = "gray",
+  isSoon = false,
+  ...props
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={styles.btnCeckBoxContainer}
+      style={style}
+      {...props}
+    >
+      <div className={styles.innerContainer}>
+        <div className={styles.right}>
+          <img src={icon} alt="" />
+          <div style={{ color: textColor }} className={styles.text}>
+            {text}
+          </div>
+        </div>
+        {isSoon && <div className={styles.left}>{isSoon ? "בקרוב!" : ""}</div>}
+      </div>
+    </button>
+  );
+};
+
+export default BtnCheckBox;
