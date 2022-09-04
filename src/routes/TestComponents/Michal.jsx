d36d0165ec0eb9b2fdfae0c99eaa903f31b63a08
michal sagi <michaldinner@gmail.com>
2022-09-04 18:15:05 +0300

    additionNew

--- src/routes/TestComponents/Michal.jsx
+++ src/routes/TestComponents/Michal.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import Confirm from '../../components/all/Confirm'
+import React from 'react';
+import Confirm from '../../components/all/Confirm';
+import AdditionNew from '../../components/all/AdditionNew';
 
 export default function Michal() {
    return (
-      <div><Confirm/></div>
+      <div> <AdditionNew /> </div>
    )
 }
