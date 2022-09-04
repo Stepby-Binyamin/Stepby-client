666ee27c38065701502932eaa95110e26ba5212b
bmzzzzzzzzzzz <400bmz@gmail.com>
2022-09-04 17:29:15 +0300

    add componnet input

---
+++ src/components/common/Input/Input.jsx
@@ -0,0 +1,10 @@
+import styles from './style.module.css'
+
+const Input = ({ name, placeholder,onChange,type }) => {
+
+  return (
+    <input className={styles.input} type={type} onChange={onChange} name={name} placeholder={placeholder} />
+  );
+}
+
+export default Input;
