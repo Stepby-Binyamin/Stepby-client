2d704df931fa67be4c9094232de935f1da60359d
oriya <oriyaroth3@gmail.com>
2022-09-04 17:49:54 +0300

    change NavLink to Link

--- src/components/common/NavLink/index.jsx
+++ src/components/common/NavLink/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import styles from './style.module.css'
 
 
-export default function NavLink({onClick, firstText, secondText}){
-    const [whoClicked, setWhoClicked] = useState()
+export default function NavLink({firstText, secondText, ...props}) {
     firstText = 'פרויקטים'
     secondText = 'תבניות'
-    onClick = (text)=>{
-        setWhoClicked(text)
-    }
-    return(
+   
+    
+    return (
         <div className={styles.main}>
-        <div className={whoClicked === firstText? styles.active: ''} onClick={()=>onClick(firstText)}>{firstText}</div>
-        <div className={whoClicked === secondText? styles.active: ''} onClick={()=>onClick(secondText)}>{secondText}</div>
+            {/* <Link to='/projects' className={window.location.pathname.split('/')[1] === 'projects'? styles.active : ''} {...props} >{firstText}</Link> */}
+            
+            <Link to='/projects' className={window.location.pathname.includes('projects')? styles.active : ''} {...props} >{firstText}</Link>
+            <Link to='/templates' className={window.location.pathname.includes('templates')?  styles.active : ''} {...props}>{secondText}</Link>
         </div>
     )
 }
