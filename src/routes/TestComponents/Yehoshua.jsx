efa8336bfa61496550f0dbb43438ef9952b0c8f1
josh goldfrad <joshgoldfrad@gmail.com>
2022-09-04 18:00:06 +0300

    updated copmonents

--- src/routes/TestComponents/Yehoshua.jsx
+++ src/routes/TestComponents/Yehoshua.jsx
@@ -6,6 +6,9 @@ export default function Yehoshua({name,num,...props}) {
    return (<>
       <div>Yehoshua</div>
       <StatusStep numOfStage="5" user="לעמרם" time="7W"    />
+<br>
+</br>
+      <StatusStep numOfStage="5" user="לך"/>
       <br></br>
       <StatusTemp/>
    </>
