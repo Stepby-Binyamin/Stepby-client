6f003bb46391bff87a74481fe56964339bf3282c
josh goldfrad <joshgoldfrad@gmail.com>
2022-09-04 17:30:26 +0300

    components done

--- src/routes/TestComponents/Yehoshua.jsx
+++ src/routes/TestComponents/Yehoshua.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import StatusStep from '../../components/all/StatusStep/StatusStep'
+import StatusTemp from '../../components/all/StatusTemp/statusBarTemplateEdit/StatusTemp'
 
-export default function Yehoshua() {
-   return (
+export default function Yehoshua({name,num,...props}) {
+   return (<>
       <div>Yehoshua</div>
+      <StatusStep numOfStage="5" user="לעמרם" time="7W"    />
+      <br></br>
+      <StatusTemp/>
+   </>
    )
 }
+
+
