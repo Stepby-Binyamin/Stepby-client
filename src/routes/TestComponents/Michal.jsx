f7911e45e6b5615b16f231e3a3029f40b26feb38
michal sagi <michaldinner@gmail.com>
2022-09-05 14:14:41 +0300

    keyboard

--- src/routes/TestComponents/Michal.jsx
+++ src/routes/TestComponents/Michal.jsx
@@ -7,9 +7,10 @@ import MoreProject from '../../components/all/MoreProject';
 import VerifyProblem from '../../components/all/VerifyProblem';
 import MoreMenuTemplate from '../../components/all/MoreMenuTemplate';
 import MoreMenuStep from '../../components/all/MoreMenuStep';
-
+import Keyboard from '../../components/all/Keyboard'
+import SubKeyboard from '../../components/all/SubKeyboard';
 export default function Michal() {
    return (
-      <div> <MoreMenuStep /> </div>
+      <div> <SubKeyboard placeholder={"טלפון"} iconSrc={"/images/icons/video.svg"} /> </div>
    )
 }
