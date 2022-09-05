3ae1868b2b912d2865a05080bb56846b617fdb7f
נהוראי <nehoraimimon@gmail.com>
2022-09-05 13:10:03 +0300

    btns box

--- src/routes/TestComponents/Nehorai.jsx
+++ src/routes/TestComponents/Nehorai.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import BtnsBox from '../../components/common/BtnsBox'
 import BtnSubmitText from '../../components/common/BtnSubmitText'
 
 
 
 export default function Nehorai() {
-   return (<>
-      <BtnSubmitText color="gray" icon={"v to text.svg"} text={"שמירה"}/>
-      <BtnSubmitText color="lite"  text={"שמירה + יצירת שלב נוסף"}/>
-   </>
+   return (
+   <BtnsBox/>
    )
 }
