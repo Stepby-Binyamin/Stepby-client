148ca15d4fa9cff83dda1a148814a9f1d88f7ccb
Yael <yael.levyy@gmail.com>
2022-09-05 09:19:39 +0300

    drawer

--- src/routes/TestComponents/Eldad.jsx
+++ src/routes/TestComponents/Eldad.jsx
@@ -3,24 +3,9 @@ import MainDrawer from "../../drawer/MainDrawer";
 import Efrat from "./Efrat";
 
 export default function Eldad() {
- const state = [
-    {
-      name: "sad",
-      img: "",
-    },
-    {
-      name: "smile",
-      img: "",
-    },
-  ];
+
   return (
      <>
-      {/* {state.map((data) => {
-     console.log(this.state);
-        console.log(data);
-        
-      return  <h3 key={data.name} style={data.name === "smile" ? "color: red" : "color: black"}>{data.nama}</h3>
-      })} */}
       <div>
         <div>sadasd</div>
         <MainDrawer Component={<Efrat />} />
