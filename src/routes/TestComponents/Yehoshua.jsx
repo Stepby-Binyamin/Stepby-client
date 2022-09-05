90a8b94a52f6df219cf46eb3877bd5167c48bfa5
josh goldfrad <joshgoldfrad@gmail.com>
2022-09-05 16:38:46 +0300

    userNumberVerification page

--- src/routes/TestComponents/Yehoshua.jsx
+++ src/routes/TestComponents/Yehoshua.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import StatusCompleted from '../../components/all/StatusCompleted'
 import StatusStep from '../../components/all/StatusStep'
 import StatusTemp from '../../components/all/StatusTemp'
+import UserNumberVerification from '../../components/all/UserNumberVerification'
 
 export default function Yehoshua({ name, num, ...props }) {
    return (<>
@@ -13,6 +14,8 @@ export default function Yehoshua({ name, num, ...props }) {
       <StatusTemp />
       <br></br>
       <StatusCompleted />
+      <br></br>
+      <UserNumberVerification phoneNum="0509876544"/>
    </>
    )
 }
