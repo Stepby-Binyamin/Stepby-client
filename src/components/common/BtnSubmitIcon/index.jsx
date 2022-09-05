2d3e45d63c71d07f2ab899f2020bfcf2b278c63e
נהוראי <nehoraimimon@gmail.com>
2022-09-05 14:46:47 +0300

    fix bug on BtnConfirm

--- src/components/common/BtnSubmitIcon/index.jsx
+++ src/components/common/BtnSubmitIcon/index.jsx
@@ -3,9 +3,9 @@ import styles from "./style.module.css"
 
 // creator: nehorai
 // chooze color: orange , gray , lite
-// vhooze icon: plus.svg, Arrow.svg , v.svg
+// chooze icon: plus.svg, Arrow.svg , v.svg
 
-export default function BtnConfirm({ color, icon, func, ...props }) {
+export default function BtnConfirm({ color, icon,func}) {
     return (
         <div className={styles.conteiner}>
             <button className={`${styles[color]} ${styles.submit}`} onClick={() => func}>
