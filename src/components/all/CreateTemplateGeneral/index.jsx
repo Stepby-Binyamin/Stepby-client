c69ef998244eb9d65139264d33c1e1aa6d401dce
michal sagi <michaldinner@gmail.com>
2022-09-05 18:24:53 +0300

    createTemplateGeneral

--- src/components/all/CreateTemplateGeneral/index.jsx
+++ src/components/all/CreateTemplateGeneral/index.jsx
@@ -3,13 +3,25 @@ import Keyboard from '../Keyboard'
 import SubKeyboard from '../SubKeyboard'
 import styles from "./style.module.css"
 import BtnSubmitText from "../../common/BtnSubmitText"
+import BtnCheckBox from '../../common/BtnCheckBox'
+import RadioBtn from '../../all/radioBtn/withoutIcon'
 
 const CreateTemplateGeneral = ({ placeholder, ...props }) => {
     const text = "זה שם שמיועד לשימוש פנימי שלך (הלקוחות שלך לא יהיו חשופים אליו)."
+    const categoris = [{ name: "עיצוב אתרים", id: 1 }, { name: "עיצוב פנים", id: 2 }, { name: "שיווק דיגיטלי", id: 3 }, { name: "אימון כושר גופני", id: 4 }]
 
     return (
         <div className={styles.container}>
             <Keyboard placeholder={"שם התבנית החדשה..."} />
+            <div className={styles.radioButton}>
+                <RadioBtn arr={['כללי', 'לקוח מסוים']} />
+                <div className={styles.rightContainer}>
+                    <img src='/images/icons/target.svg' alt="" />
+                    <div className={styles.text}>תפוצה</div>
+                </div>
+            </div>
+
+            {categoris.map(elem => <BtnCheckBox name={elem.name} id={elem.id} />)}
 
 
             <div className={styles.btn}> <BtnSubmitText color={"gray"} text="שמירה" icon={"v to text.svg"} /> </div>
