2eaa663ec09c9dfab5fc452ee4d7ac1c6489f5e7
josh goldfrad <joshgoldfrad@gmail.com>
2022-09-05 13:02:35 +0300

    changed names of components to index.jsx

--- src/routes/TestComponents/Yehoshua.jsx
+++ src/routes/TestComponents/Yehoshua.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import StatusCompleted from '../../components/all/StatusCompleted/StatusCompleted'
-import StatusStep from '../../components/all/StatusStep/StatusStep'
-import StatusTemp from '../../components/all/StatusTemp/statusBarTemplateEdit/StatusTemp'
+import StatusCompleted from '../../components/all/StatusCompleted'
+import StatusStep from '../../components/all/StatusStep'
+import StatusTemp from '../../components/all/StatusTemp'
 
-export default function Yehoshua({name,num,...props}) {
+export default function Yehoshua({ name, num, ...props }) {
    return (<>
       <div>Yehoshua</div>
-      <StatusStep numOfStage="5" user="לעמרם" time="7W"    />
-<br>
-</br>
-      <StatusStep numOfStage="5" user="לך"/>
+      <StatusStep numOfStage="5" user="לעמרם" time="7W" />
       <br></br>
-      <StatusTemp/>
+      <StatusStep numOfStage="5" user="לך" />
       <br></br>
-      <StatusCompleted/>
+      <StatusTemp />
+      <br></br>
+      <StatusCompleted />
    </>
    )
 }
