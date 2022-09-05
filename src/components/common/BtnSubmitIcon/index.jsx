3e4edf7fb5ffea276d75030ace3b0ba7edbc86dd
נהוראי <nehoraimimon@gmail.com>
2022-09-05 15:53:00 +0300

    bugs1

--- src/components/common/BtnSubmitIcon/index.jsx
+++ src/components/common/BtnSubmitIcon/index.jsx
@@ -5,14 +5,8 @@ import styles from "./style.module.css"
 // chooze color: orange , gray , lite
 // chooze icon: plus.svg, Arrow.svg , v.svg
 
-<<<<<<< HEAD
-export default function BtnConfirm({ color, icon, func, ...props }) {
-    return (<>
-        <div className={styles.line}></div>
-=======
 export default function BtnConfirm({ color, icon,func}) {
-    return (
->>>>>>> d62ef21f22b1dbe3689b306dba227dd863831e03
+    return (<>
         <div className={styles.conteiner}>
             <button className={`${styles[color]} ${styles.submit}`} onClick={() => func}>
                 <img src={"/images/icon-btns/" + icon} alt="" />
