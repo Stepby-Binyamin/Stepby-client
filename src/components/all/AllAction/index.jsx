bce20ee02bc69bb0aebf573044b04b351dbb28c2
michal sagi <michaldinner@gmail.com>
2022-09-06 18:55:56 +0300

    change text

--- src/components/all/AllAction/index.jsx
+++ src/components/all/AllAction/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import BtnIcon from '../../common/BtnIcon'
 import styles from "./style.module.css"
+import { languages } from '../../../functions/languages'
 
-const AllAction = ({ style = {}, func1, func2, func3, ...props }) => {
-
+const AllAction = ({ style = {}, newTempFunc, newUserFunc, projectToUserFunc, ...props }) => {
+   const dict = languages[0].dict;
    return (
       <div className={styles.additionNew}>
-         <div className={styles.btn}><BtnIcon icon='/images/icons/newTemplate.svg' text='תבנית חדשה' onClick={func1} />      </div>
-         <div className={styles.btn1}><BtnIcon icon='/images/icons/newCustomer.svg' text='לקוח חדש' onClick={func2} />      </div>
-         <div><BtnIcon icon='/images/icons/newProject.svg' text='פרויקט חדש ללקוח קיים ' onClick={func3} />      </div>
+         <div className={styles.btn}><BtnIcon icon='/images/icons/newTemplate.svg' text={dict.NEW_TEMPLATE} onClick={newTempFunc} />      </div>
+         <div className={styles.btn1}><BtnIcon icon='/images/icons/newCustomer.svg' text={dict.NEW_CUSTOMER} onClick={newUserFunc} />      </div>
+         <div><BtnIcon icon='/images/icons/newProject.svg' text={dict.NEW_PROJECT} onClick={projectToUserFunc} />      </div>
       </div>
    )
 }
