f4da3d0543ae7d49b320d5bea4f14de2dfcbfc11
Yael <yael.levyy@gmail.com>
2022-09-06 16:11:46 +0300

    drewer css update

--- src/drawer/MainDrawer.jsx
+++ src/drawer/MainDrawer.jsx
@@ -11,8 +11,10 @@ export default function MainDrawer({ children }) {
         className={styles[drawer.drawer ? "containerOn" : "containerOff"]}
         onClick={() => drawer.setDrawer("")}
       >
-        <div className={styles[drawer.drawer ? "popUp" : "popDown"]}>
-          <div onClick={() => drawer.setDrawer()} className={styles.lower} />
+        <div className={styles[drawer.drawer ? "popUp" : "popDown"]} onClick={(e)=>e.stopPropagation()}>
+          <div style={{'width': '100%', 'height': '36px'}} onClick={() => drawer.setDrawer()}>
+          <div className={styles.lower} />
+          </div>
           {drawer.drawerContent}
         </div>
       </div>
