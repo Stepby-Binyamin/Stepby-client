12d4129f5d717eb7f4247282bd6fbcd76ea71ed5
Shaul Attie <solyattie13@gmail.com>
2022-09-06 18:12:44 +0300

    BtnCheckBox CSS label width cahnge to 80%, changes on fakeProjects for testing of pageBExample2, creation of pageBExample2, changes on pageBExample1, changes on Shaul

--- src/routes/TestComponents/Shaul.jsx
+++ src/routes/TestComponents/Shaul.jsx
@@ -9,34 +9,36 @@ import HeaderTitle from '../../components/common/HeaderTitle'
 import Input from '../../components/common/Input/Input'
 
 import BExample1 from '../../pages/project/BExample1'
+import BExample2 from '../../pages/project/BExample2'
 
 export default function Shaul() {
 
    //BtnCheckBox - how to set <BtnCheckBox /> example - dont DELETE IT
 
-   // const dataTest = [
-   //    { title: "A", isActive: false },
-   //    { title: "B", isActive: false },
-   //    { title: "C", isActive: false },
-   //    { title: "D", isActive: false },]
+   const dataTest = [
+      { title: "A", isActive: false },
+      { title: "B", isActive: false },
+      { title: "C", isActive: false },
+      { title: "D", isActive: false },]
 
-   // useEffect(() => {
-   //    setData(dataTest)
-   // }, [])
+   useEffect(() => {
+      setData(dataTest)
+   }, [])
 
-   // const [data, setData] = useState()
+   const [data, setData] = useState()
 
-   // const handleClick = (name) => {
-   //    const result = data.map(elem => elem.title === name ? ({ ...elem, isActive: !elem.isActive }) : elem)
-   //    setData(result)
-   // }
+   const handleClick = (name) => {
+      const result = data.map(elem => elem.title === name ? ({ ...elem, isActive: !elem.isActive }) : elem)
+      setData(result)
+   }
 
    return (
       <>
          {/* <HeaderLogo isArrow={isArrow} isHeaderSet={isHeaderSet} /> */}
          {/* <HeaderTitle drawerContent={drawerContent} isArrow={isArrow} isHamburguer={isHamburguer} /> */}
-         {/* {data?.map(elem => <BtnCheckBox name={elem.title} id={elem.title} key={elem.title} handleClick={handleClick} isActive={elem.isActive} />)} */}
-         <BExample1 />
+         {data?.map(elem => <BtnCheckBox name={elem.title} id={elem.title} key={elem.title} handleClick={handleClick} isActive={elem.isActive} />)}
+         {/* <BExample1 />
+         <BExample2 /> */}
       </>
    )
 }
