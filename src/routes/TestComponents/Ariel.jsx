9c6513c7e7f8aaab66a773b02f6348475ecf8bcc
ariel gold <ariel6185@gmail.com>
2022-09-07 14:26:31 +0300

    draft12 home consts

--- src/routes/TestComponents/Ariel.jsx
+++ src/routes/TestComponents/Ariel.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-// import ListItem from '../../components/common/ListItem'
+import ListItem from '../../components/common/ListItem'
 import HomeProject from '../../pages/home/HomeProject'
 
 export default function Ariel() {
    return (
       <>
          {/* <ListItem /> */}
+         <ListItem
+                              // key={}
+                              status={"biz"}  // item.steps[0].status
+                              mainTitle={"dsdsd"}
+                              // secondaryTitle={item.name}
+                              sconderyBoldTitle={"item.steps[0].name"}  //get current temp
+                              time={"2d"}
+                              link={`/project/`}  //path
+                           />
         <HomeProject />
       </>
    )
