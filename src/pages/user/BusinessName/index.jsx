09ec7c87d8d935bde8f02732353f9e5d6f359ef1
oriya <oriyaroth3@gmail.com>
2022-09-07 11:35:52 +0300

    ok

--- src/pages/user/BusinessName/index.jsx
+++ src/pages/user/BusinessName/index.jsx
@@ -7,43 +7,46 @@ import { useContext } from 'react'
 import mainContext from '../../../context/mainContext'
 import { useEffect } from 'react'
 import BtnSubmitIcon from '../../../components/common/BtnSubmitIcon'
-import { useNavigate } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 export default function BusinessName({ newUser = true, BusinessName }) {
 
-    const { header } = useContext(mainContext)
-    const navigate = useNavigate(),
-    [input, setInput] = useState()
+    const { header } = useContext(mainContext),
+        navigate = useNavigate(),
+        [input, setInput] = useState();
+    let userData = useLocation();
+    userData = userData.state;
 
     useEffect(() => {
         header.setIsTitle(false)
+        console.log(userData);
     }, [])
 
-    const handleChange = (e)=>{
-        setInput(e.target.value)
+    const handleChange = (e) => {
+        userData.businessNm = e.target.value
         console.log(0);
     }
 
-    const handleClickNew = ()=>{
-        navigate('/category', {state: input})
-        console.log(input);
+    const handleClickNew = () => {
+        navigate('/category', { state: userData })
+        console.log(userData);
 
     }
 
-    const handleClickExist = ()=>{
+    const handleClickExist = () => {
         console.log(input);
     }
 
     return (
         <div className={styles.box}>
-            <div  className={styles.title}>
-                <UserTitle text={languages[0].dict.BUSINESS_NAME_HEADER}/>
+            <div className={styles.title}>
+                <UserTitle text={languages[0].dict.BUSINESS_NAME_HEADER} />
             </div>
             <div className={styles.input}>
                 <Input type='text' onChange={handleChange} placeholder={newUser ? languages[0].dict.YOUR_BUSINESS_NAME : ''} defaultValue={!newUser ? BusinessName : ''} />
             </div>
             <div className={styles.btn}>
-                <BtnSubmitIcon color='orange' icon={newUser ? 'Arrow.svg':'v to text.svg'} func={newUser?handleClickNew:handleClickExist} />
+                <BtnSubmitIcon color='orange' icon={newUser ? 'Arrow.svg' : 'v to text.svg'} func={newUser ? handleClickNew : handleClickExist} />
             </div>
         </div>
     )
