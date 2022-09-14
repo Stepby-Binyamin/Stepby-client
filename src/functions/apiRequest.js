63f40b90169288015436ccda759d620e532bf29a
oriya <oriyaroth3@gmail.com>
2022-09-14 10:12:29 +0300

    axios changes

--- src/functions/apiRequest.js
+++ src/functions/apiRequest.js
@@ -1,30 +1,26 @@
 import axios from "axios"
 
-// axios.create({
-//   baseURL: "http://localhost:5000"
-// });
-
 axios.defaults.baseURL = "http://localhost:5000" 
 
 export const setToken = (token)=>{
     // when you do logout pass the parameter as an empty string
     axios.defaults.headers.common['Authorization'] =  token //AUTH_TOKEN
 }
-const apiCalls = async (url, method, data) => {
-    let res;
-    await axios({
-        method,
-        url,
-        data
-    })
-        .then( async (response) => {
-            res = response.data
+
+
+const apiCalls = async (method, url, data) => {
+    try{
+        const res = await axios({
+            method,
+            url,
+            data
         })
-        .catch( (error) => {
-            res = error.message
-            // throw error.message
-        })
-        return res
+        return res.data
+        }
+    
+        catch(error) {
+            throw error
+        }
 }
 
 export default apiCalls
