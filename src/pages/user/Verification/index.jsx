ecc2192543b22fe55bae0d4195ce9d2c96bd11db
oriya <oriyaroth3@gmail.com>
2022-09-15 18:03:46 +0300

    asdfghjkjhgfds

--- src/pages/user/Verification/index.jsx
+++ src/pages/user/Verification/index.jsx
@@ -15,7 +15,7 @@ import apiCalls from '../../../functions/apiRequest'
 
 export default function Verification() {
   const { header } = useContext(mainContext);
-  const {userData, setUserData} = useContext(userContext)
+  const { userData, setUserData } = useContext(userContext)
   const navigate = useNavigate();
   const [counter, setCounter] = useState(0);
   const location = useLocation();
@@ -32,13 +32,16 @@ export default function Verification() {
   const ilPhoneNum = `${start}-${end}`
 
   const sendCode = async () => {
-    if(localStorage.token) setToken(localStorage.token)
+    if (localStorage.token) setToken(localStorage.token)
     await apiCalls("post", "/user/send-code", { phoneNumber: location.state })
-    .then(async(res)=>{
-      console.log(1234, res);
-      res.categories?
-      await navigate('/projects'):console.log(res)})
-    .catch((err)=>console.log(err))
+      .then((res) => {
+        console.log(1234, res)
+        if(res.categories){
+          setUserData(res) 
+           navigate('/projects')
+        } 
+      })
+      .catch((err) => console.log(err))
   }
 
   useEffect(() => {
@@ -59,11 +62,11 @@ export default function Verification() {
         console.log(result)
         localStorage.user = JSON.stringify(result.user)
         localStorage.token = result.token
-        result.user.firstName? navigate('/projects') : navigate('/user-name')
+        result.user.firstName ? navigate('/projects') : navigate('/user-name')
       })
       .catch(() => setWrongPassword(true))
-      console.log(localStorage.user);
-      
+    console.log(localStorage.user);
+
   }
 
   return (
