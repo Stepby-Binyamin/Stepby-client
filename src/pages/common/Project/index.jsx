5e78b4954457aaf5dab63a3c50c86a181fbfaac5
yosefus <yosalsoni@gmail.com>
2022-09-20 22:28:25 +0300

    add state to listItem

--- src/pages/common/Project/index.jsx
+++ src/pages/common/Project/index.jsx
@@ -178,7 +178,7 @@ export default function Project({ mode }) {
                         down={downMove}
                         id={step._id}
                         link={nav({ mode, curr, step })}
-                        linkState={{ tempName: curr.name, step, stepId: step._id }}
+                        linkState={{ tempName: curr.name, step, stepId: step._id, curr }}
 
                     />)}
                 {curr.steps?.length < 1 && <UiDirectionText mainTitle={STEP_BY_STEP} text1={PRESS_ON} text2={ADD_STEP} />}
