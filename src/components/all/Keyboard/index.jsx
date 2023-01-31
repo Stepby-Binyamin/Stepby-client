f52f031c242d8a173ede343e92d02b789368cdce
efrat5454 <efrat5454@gmail.com>
2023-01-31 11:59:46 +0200

    StepBasics

--- src/components/all/Keyboard/index.jsx
+++ src/components/all/Keyboard/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
+import { useEffect } from 'react'
 import { useState } from 'react'
 import styles from "./style.module.css"
 
-const Keyboard = ({ placeholder, onChange, required, defaultValue, name, missingData = false, ...props }) => {
+const Keyboard = ({ placeholder, onChange, required, defaultValue, name, missingData = false, toClean = false, ...props }) => {
     const [value, setValue] = useState(defaultValue)
+    useEffect(() => {
+        if (toClean)
+            setValue('')
+    }, [toClean])
+
     const change = (e) => {
         onChange && onChange(e)
         setValue(e.target.value)
     }
+
     return (
         <div className={styles.container}>
             <input
