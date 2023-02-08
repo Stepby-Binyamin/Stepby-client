02b581e8c117f868b21035587808a5bf4905c218
efrat5454 <efrat5454@gmail.com>
2023-02-08 18:38:08 +0200

    missingData

--- src/components/common/Input/Input.jsx
+++ src/components/common/Input/Input.jsx
@@ -1,9 +1,10 @@
 import styles from './style.module.css'
 
-const Input = ({ name, placeholder, onChange, type, ...props }) => {
+const Input = ({ name, placeholder, onChange, type, missingData = false, ...props }) => {
 
   return (
-    <input className={styles.input}
+    <input
+      className={`${styles.input} ${missingData && styles.missing_data}`}
       type={type}
       onChange={onChange}
       name={name}
